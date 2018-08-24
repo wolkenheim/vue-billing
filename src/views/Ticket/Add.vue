f95a26cba8175f241640e4a6cac8738dad55fe58
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-08-24 20:48:24 +0200

    Ticket Form emits event so that parent closes modal / dialog window

--- src/views/Ticket/Add.vue
+++ src/views/Ticket/Add.vue
@@ -2,45 +2,28 @@
   <v-layout row justify-center>
     <v-dialog v-model="dialog" persistent max-width="500px">
       <v-btn slot="activator" color="primary" dark>Neues Ticket</v-btn>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Neues Ticket erstellen</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field v-model="ticket.title" label="Titel" required></v-text-field>
-                <v-text-field v-model="ticket.description" label="description" required></v-text-field>
-                <v-text-field v-model="ticket.hours" label="hours" required></v-text-field>
-                <v-text-field v-model="ticket.minutes" label="minutes" required></v-text-field>
-                <v-text-field v-model="ticket.customer_id" label="minutes" required></v-text-field>
-              </v-flex>
-
-            </v-layout>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+      <ticket-form :customer-id="customerId" :ticket="ticket"></ticket-form>
     </v-dialog>
   </v-layout>
 </template>
 
 <script>
   import Ticket from '../../models/ticket.js';
+  import TicketForm from './Form';
+  import EventBus from '../../components/EventBus.js';
   export default {
     name: 'TicketAdd',
+    components: {
+      TicketForm
+    },
     props: ['customerId'],
     data: () => ({
       ticket: {},
       dialog: false
     }),
     created(){
+      EventBus.$on('closeDialog', () => {this.dialog = false});
+
       this.ticket = new Ticket();
       this.ticket.customer_id = this.customerId;
     },
---
+++ src/views/Ticket/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">Neues Ticket erstellen</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <v-flex xs12>
+            <v-text-field v-model="ticket.title" label="Titel" required></v-text-field>
+          </v-flex>
+          <v-flex xs12>
+            <v-textarea v-model="ticket.description" label="description" required></v-textarea>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field v-model="ticket.hours" type="number" min="0" label="hours" required></v-text-field>
+          </v-flex>
+          <v-flex xs6>
+            <v-text-field v-model="ticket.minutes" type="number" min="0" max="59"  label="minutes" required></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <small>*indicates required field</small>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue darken-1" flat @click.native="closeDialog()">Close</v-btn>
+      <v-btn color="blue darken-1" flat @click.native="saveTicket()">Save</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  import EventBus from '../../components/EventBus.js';
+  export default {
+    name: 'TicketForm',
+    props: ['customerId', 'ticket'],
+    methods: {
+      saveTicket(){
+        console.log("save ticket");
+        this.closeDialog();
+      },
+      closeDialog(){
+        EventBus.$emit("closeDialog");
+      }
+    }
+  }
+</script>
